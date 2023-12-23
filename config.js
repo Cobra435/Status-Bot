5166b9efb32d5a14e9777ca5c1f87b5da81d5805
Its-DisguisedHyper <disguisedhyper@disguiseddevelopment.xyz>
2023-12-23 01:21:43 +0000

    Updates

--- config.js
+++ config.js
@@ -1,21 +1,22 @@
 module.exports = {
-  token: 'MTE3OTQwMzM0Mzc3MzMwNjkzMA.GOe53F.ROyXp0QZiuvWI2wFa5HNQqeV5ZUOemNbtv8jXQ',  // Bot Token
-  channelID: '1187510784822104174',  // Discord Channel ID for Status Messages
-  messageID: '1187919734528823356',  // Discord Message ID for Updating Status Messages
+  token: '',  // Bot Token
+  channelID: '',  // Discord Channel ID for Status Messages
+  messageID: '',  // Discord Message ID for Updating Status Messages
   port: 3000,  // Port for the server
-  roleID: '1174187383042879549',  // Role ID to Mention when Server is Down
-  guildID: '1174185500517609562',  // Discord Guild ID
-  allowedRoleID: '1174187383042879549',  // Allowed Role ID for Using Commands
+  roleID: '',  // Role ID to Mention when Server is Down
+  guildID: '',  // Discord Guild ID
+  allowedRoleID: '',  // Allowed Role ID for Using Commands
   sendDirectMessages: true,  // Option to Send Direct Messages
-  logChannelId: '1185591705349652562',  // Discord Log Channel ID
+  logChannelId: '',  // Discord Log Channel ID
   websites: [
     {
       name: 'Website 1',
-      url: 'http://petro.projectphil.co.uk:4005',
+      url: 'http://example.com',  // Replace with your website URL
       status: 'Online - ✅',
       dm: false,
       downtime: 0,
     },
+    // Add more websites as needed
   ],  // Configuration for Monitoring Websites
   commands: {
     disableDM: { name: 'disabledm', description: 'Disable direct messages'},
